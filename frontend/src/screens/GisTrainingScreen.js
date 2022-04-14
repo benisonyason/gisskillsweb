05b6984376d3f03308e575cadd098792d3fec6e7
benisonyason <benisonyason91@gmail.comm>
2022-04-14 19:03:10 +0100

    git

--- frontend/src/screens/GisTrainingScreen.js
+++ frontend/src/screens/GisTrainingScreen.js
@@ -20,7 +20,7 @@ export default function GisTrainingScreen(props) {
             <h1 style={{ textShadow: '2px 2px 5px red', fontSize: '25px', textAlign: 'center' }}>Improve your GIS Skills with Free Basic Certificate</h1>
           </div>
           <div className='row center'>
-            <h1><strong style={{ fontSize: '40px' }}><button className='courses'><a href='https://chat.whatsapp.com/CSOwtcmkB9K0NS95XsG3S3'>Join Batch 004</a></button></strong></h1>
+            <h1><strong style={{ fontSize: '40px' }}><button className='courses'><a href='https://chat.whatsapp.com/HX3vdmoyS61LSkdiKLGJvx'>Join Batch 005</a></button></strong></h1>
           </div>
           <div className='row center'>
             <h2>Lessons Start 7pm(GMT +1) 14/04/2022</h2>
