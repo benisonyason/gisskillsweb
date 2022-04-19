6266f2f811a10c051a987ffe1dd265bef1d7286a
benisonyason <benisonyason91@gmail.comm>
2022-04-19 07:33:16 +0100

    git commit

--- frontend/src/screens/GisResourceScreen.js
+++ frontend/src/screens/GisResourceScreen.js
@@ -6,8 +6,8 @@ export default function GisResourceScreen(props) {
     return (
         <div>
             <Helmet>
-                <title>Register for Niger Maps Account</title>
-                <meta name="description" content="Register Now for Niger Maps account and download GIS Maps for all places in Nigeria" />
+                <title>Download Dataset</title>
+                <meta name="description" content="Download tutorial dataset for our training manual... with updated administrative, rpads, settlements, rivers and water bodies layer files" />
                 <link rel="canonical" href="/register" />
             </Helmet>
 
@@ -15,13 +15,15 @@ export default function GisResourceScreen(props) {
                 <h1 style={{ fontSize: '20px' }}>Tutorial Data Download</h1><br />
             </div>
             <div className='row center'>
-                <p>Follow step by step guide as in your training manual with our example dataset.</p>
+                <p>Follow step by step guide as in your training manual with our example dataset and updated administrative Database.
+                    
+                </p>
             </div>
             <ul className='row center'>
-                <li><a href="https://paystack.com/pay/nigermapsbasicdata" ><button style={{ fontSize: '16px' }} className="primary">Download Basic GIS Tutorial Dataset <i>(<del>&#8358;</del>2,000)</i></button></a></li>
+                <li><a href="https://paystack.com/pay/nigermapsbasicdata" ><button style={{ fontSize: '16px' }} className="primary">Download Basic GIS Tutorial Dataset <i>(<del>&#8358;</del>5,000)</i></button></a></li>
             </ul>
             <ul className='row center'>
-                <li><a href="https://paystack.com/pay/nigermapsdata" ><button style={{ fontSize: '16px' }} className="primary">Download Advance GIS Tutorial Dataset <i>(<del>&#8358;</del>5,000)</i></button></a></li>
+                <li><a href="https://paystack.com/pay/nigermapsdata" ><button style={{ fontSize: '16px' }} className="primary">Download Advance GIS Tutorial Dataset <i>(<del>&#8358;</del>10,000)</i></button></a></li>
             </ul>
         </div>
     );
--- frontend/src/screens/GisTrainingScreen.js
+++ frontend/src/screens/GisTrainingScreen.js
@@ -20,10 +20,10 @@ export default function GisTrainingScreen(props) {
             <h1 style={{ textShadow: '2px 2px 5px red', fontSize: '25px', textAlign: 'center' }}>Improve your GIS Skills with Free Basic Certificate</h1>
           </div>
           <div className='row center'>
-            <h1><strong style={{ fontSize: '40px' }}><button className='courses'><a href='https://chat.whatsapp.com/HX3vdmoyS61LSkdiKLGJvx'>Join Batch 005</a></button></strong></h1>
+            <h1><strong style={{ fontSize: '40px' }}><button className='courses'><a href='https://chat.whatsapp.com/LbmTo2E8fJj1EMyXaLh7DI'>Join Batch 005</a></button></strong></h1>
           </div>
           <div className='row center'>
-            <h2>Lessons Start 7pm(GMT +1) 14/04/2022</h2>
+            <h2>Lessons Start Every 7pm(GMT +1) 23/04/2022</h2>
           </div>
 
           <Carousel
